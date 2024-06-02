4588d941bc00da979f039a8304d3576a6aa2b675
Benjamin Fallar III <bfallar3@hotmail.com>
2024-06-02 14:13:19 +0800

    update base url in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://akita-backend-service-latest.onrender.com/api/todo'
+  baseUrl: 'https://akita-backend-service-latest.onrender.com/api/todos'
 };
